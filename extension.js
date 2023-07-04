020599a76842c259cd30d154d725c0800e0d8b5d
thufo <thufo@qq.com>
2023-07-04 23:53:25 +0800

    修正,,有bug

--- extension.js
+++ extension.js
@@ -6,37 +6,32 @@ function activate(context) {
     const { commands, window } = vscode;
 
     const regexReplacements = [
-        {
-            title: '消息替换',
-            replacements: [
-                {
-                    "regex": /<IMG:(.*?):(.*?):(.*?):(.*?)>/gi,
-                    "replacement": "{F=$2;I=$1;X=$3;X=$4}",
-                    "pararms": [
-                        {
-                            "replaceId": 2,
-                            "operation": "=",
-                            "item": {
-                                "18": "images.wil"
-                            }
-                        },
-                        {
-                            "replaceId": 1,
-                            "operation": "+",
-                            "item": 20
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            title: '字符串转换2',
-            replacements: [
-                { regex: /321/g, replacement: 'cba' },
-                { regex: /cba/g, replacement: '321' }
-            ]
-        }
-    ];
+		{
+			"title": "消息替换",
+			"replacements": [
+				{
+					"regex": "/<IMG:(.*?):(.*?):(.*?):(.*?)>/gi",
+					"replacement": "{F=$2;I=$1;X=$3;X=$4}",
+					"pararms": [
+						{
+							"replaceId": 2,
+							"operation": "=",
+							"item": {
+								"18": {
+									"value": "images.wil",
+									"chidPararms": [{
+										"replaceId":1,
+										"operation": "+",
+										"value":1500
+									}]
+								}
+							}
+						}
+					]
+				}
+			]
+		}
+	];
     const workspacePath = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : '';
     regexReplacements.forEach((rule, index) => {
         const command = commands.registerCommand(`extension.regexReplace${index + 1}`, async () => {
@@ -72,12 +67,28 @@ function activate(context) {
                 
                 rule.replacements.forEach(({ regex, replacement, pararms }) => {
                     const replacementMap = {};
-                    if (pararms) {
-                        pararms.forEach(({ replaceId, operation, item }) => {
-                            replacementMap[replaceId] = [operation, item];
-                        });
-                    }
-                    console.log(replacementMap)
+					if (pararms) {
+						pararms.forEach(({ replaceId, operation, item }) => {
+							if (typeof item === 'object' && item !== null) {
+								if (item.hasOwnProperty(replaceId)) {
+									const { value, chidPararms } = item[replaceId];
+									if (chidPararms) {
+										chidPararms.forEach(({ replaceId: childReplaceId, operation: childOperation, value: childValue }) => {
+											if (childOperation === '+') {
+												replacementMap[childReplaceId] = [childOperation, childValue];
+											} else if (childOperation === '=') {
+												replacementMap[childReplaceId] = childValue;
+											}
+										});
+									} else {
+										replacementMap[replaceId] = value;
+									}
+								}
+							} else {
+								replacementMap[replaceId] = [operation, item];
+							}
+						});
+					}
                     text = text.replace(regex, (match, ...args) => {
                         let replaced = replacement;
                         Object.entries(replacementMap).forEach(([replaceId, [operation, item]]) => {

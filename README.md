37fed00c76777f9584b6b93c176be44b6f131e16
thufo <thufo@qq.com>
2023-07-04 19:21:43 +0800

    创建github

---
+++ README.md
@@ -0,0 +1,7 @@
+# My VS Code Extension
+
+This is a simple VS Code extension for replacing strings in a document with a set of regular expressions.
+
+## Usage
+
+Right-click on the document and choose "GOM转91M2" -> "字符串转换1" or "字符串转换2" to replace strings in the document.
---
+++ extension.js
@@ -0,0 +1,114 @@
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+
+function activate(context) {
+    const { commands, window } = vscode;
+
+    const regexReplacements = [
+        {
+            title: '消息替换',
+            replacements: [
+                {
+                    "regex": /<IMG:(.*?):(.*?):(.*?):(.*?)>/gi,
+                    "replacement": "{F=$2;I=$1;X=$3;X=$4}",
+                    "pararms": [
+                        {
+                            "replaceId": 2,
+                            "operation": "=",
+                            "item": {
+                                "18": "images.wil"
+                            }
+                        },
+                        {
+                            "replaceId": 1,
+                            "operation": "+",
+                            "item": 20
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            title: '字符串转换2',
+            replacements: [
+                { regex: /321/g, replacement: 'cba' },
+                { regex: /cba/g, replacement: '321' }
+            ]
+        }
+    ];
+    const workspacePath = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : '';
+    regexReplacements.forEach((rule, index) => {
+        const command = commands.registerCommand(`extension.regexReplace${index + 1}`, async () => {
+            const editor = window.activeTextEditor;
+            if (editor) {
+                const document = editor.document;
+                let text = document.getText();
+
+                let file_replacements;
+                try {
+                    let fileRegexPth = path.join(workspacePath, 'commandsRegex.json');
+
+                    // 检查文件fileRegexPth是否存在
+                    if (fs.existsSync(fileRegexPth)) {
+                        file_replacements = JSON.parse(fs.readFileSync(fileRegexPth, 'utf-8'));
+                        // 检查当前index的规则是否存在
+                        if (file_replacements[index]) {
+                            rule.replacements = file_replacements[index].replacements.map(({ regex, replacement, pararms }) => {
+                                // Remove the leading and trailing slashes and split into pattern and flags
+                                const [pattern, flags] = regex.toString().slice(1, -1).split('/');
+                                return {
+                                    regex: new RegExp(pattern, flags),
+                                    replacement,
+                                    pararms
+                                };
+                            });
+                        }
+                    }
+                } catch (err) {
+                    console.error('Failed to load or parse commandsRegex.json:', err);
+                    file_replacements = [];
+                }
+                
+                rule.replacements.forEach(({ regex, replacement, pararms }) => {
+                    const replacementMap = {};
+                    if (pararms) {
+                        pararms.forEach(({ replaceId, operation, item }) => {
+                            replacementMap[replaceId] = [operation, item];
+                        });
+                    }
+                    console.log(replacementMap)
+                    text = text.replace(regex, (match, ...args) => {
+                        let replaced = replacement;
+                        Object.entries(replacementMap).forEach(([replaceId, [operation, item]]) => {
+                            if (args[parseInt(replaceId) - 1] && operation === '=') {
+                                replaced = replaced.replace(new RegExp(`\\$${replaceId}`, 'g'), item[args[parseInt(replaceId) - 1]] || args[parseInt(replaceId) - 1]);
+                            } else if (args[parseInt(replaceId) - 1] && operation === '+') {
+                                replaced = replaced.replace(new RegExp(`\\$${replaceId}`, 'g'), (parseInt(args[parseInt(replaceId) - 1]) + parseInt(item)).toString());
+                            }
+                        });
+                        return replaced;
+                    });
+                });
+                
+
+                const all = new vscode.Range(
+                    document.positionAt(0),
+                    document.positionAt(text.length)
+                );
+
+                editor.edit((editBuilder) => {
+                    editBuilder.replace(all, text);
+                });
+            }
+        });
+        context.subscriptions.push(command);
+    });
+}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+};
